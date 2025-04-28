66778c40b3881e8003a3f28f2b1b3f8d4c68cecf
mxs <mxs@dismail.de>
2025-04-28 12:22:39 +0100

    eval_train: small fixes

--- eval_train.py
+++ eval_train.py
@@ -6,12 +6,13 @@ import util
 
 
 @torch.no_grad()
-def eval(model, data):
+def eval(model, data, log_extra=dict()):
   model.eval()
   device = util.device()
   for batch in tqdm(data):
     batch = batch.to(device)
-    util.log(...)
+    loss = ...
+    util.log({'test/loss': loss.item(), **log_extra})
 
 
 class Trainer:
@@ -28,7 +29,7 @@ class Trainer:
   def train(self, data_train, data_test):
     self.model.train()
     device = util.device()
-    for epoch in self.num_epochs:
+    for epoch in range(self.num_epochs):
       for b, batch in enumerate(data_train):
         batch = batch.to(device)
         loss = ...
@@ -39,9 +40,14 @@ class Trainer:
         every_n = lambda ref: b % ref == 0 and batch.shape[0] == self.batch_size
         if every_n(self.checkpoint_freq):
           torch.save(
-            self.model.state_dict(), os.path.join(self.checkpoint_dir, 'checkpoint.pt')
+            self.model.state_dict(),
+            os.path.join(self.checkpoint_dir, f'checkpoint_ep{epoch}_step{b}.pt'),
           )
+        step_stats = {
+          'epoch': epoch,
+          'batch': epoch * len(data_train) + b,
+        }
         if every_n(self.eval_freq):
-          eval(self.model, data_test)
+          eval(self.model, data_test, step_stats)
         if every_n(self.log_freq):
-          util.log(...)
+          util.log({'train/loss': loss.item(), **step_stats})
